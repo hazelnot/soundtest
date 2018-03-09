9ea83c49ec2e92f5d58ac2022a662acc99dc2c78
hazelnot <scrabcrab@hotmail.com>
2018-03-09 21:35:29 +0000

    Initial commit

---
+++ slide.js
@@ -0,0 +1,11 @@
+//set default slider value
+window.onload = function(){
+	$(".slider1").val(0);
+}
+
+//print slider value
+function SliderVal(){
+  var slider = $(".slider1").val();
+  console.log(slider);
+};
+
